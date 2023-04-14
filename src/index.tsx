d2607ae38dc21ae98a7b21783a2182e0f87a22d8
Behbod Babai <behibabai@gmail.com>
2023-04-14 03:36:40 -0700

    implementing In-Browser transpiling using esbuild

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,35 @@
-import { useState } from 'react';
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+    const ref = useRef<any>();
     const [input, setInput] = useState('');
     const [code, setCode] = useState('');
 
-    const onClick = () => {
-        console.log(input);
-    }
+    const startService = async () => {
+        ref.current = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm'
+        });
+    };
+
+    useEffect(() => {
+        startService();
+    }, []);
+
+    const onClick = async () => {
+        if (!ref.current){
+            return;
+        }
+
+        const result = await ref.current.transform(input, {
+            loader: 'jsx',
+            target: 'es2015'
+        });
+
+        setCode(result.code);
+    };
     
     return <div>
         <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
