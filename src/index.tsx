501da7672a8eb08a6a1d9e002a0674ac3858b324
Behbod Babai <behibabai@gmail.com>
2023-04-15 04:10:59 -0700

    Adding Dynamic Fetching and Loading of NPM modules with the help of UNPKG.com

--- src/index.tsx
+++ src/index.tsx
@@ -1,51 +1,58 @@
 import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 const App = () => {
-    const ref = useRef<any>();
-    const [input, setInput] = useState('');
-    const [code, setCode] = useState('');
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
 
-    const startService = async () => {
-        ref.current = await esbuild.startService({
-            worker: true,
-            wasmURL: '/esbuild.wasm'
-        });
-    };
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
 
-    useEffect(() => {
-        startService();
-    }, []);
+  useEffect(() => {
+    startService();
+  }, []);
 
-    const onClick = async () => {
-        if (!ref.current){
-            return;
-        }
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
 
-        const result = await ref.current.build({
-            entryPoints: ['index.js'],
-            bundle: true,
-            write: false,
-            plugins: [unpkgPathPlugin()]
-        });
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
 
-        // console.log(result);
+    // console.log(result);
 
-        setCode(result.outputFiles[0].text);
-    };
-    
-    return <div>
-        <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
-        <div>
-            <button onClick={onClick}>Submit</button>
-        </div>
-        <pre>{code}</pre>
-    </div>;
+    setCode(result.outputFiles[0].text);
+  };
+
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
 };
 
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-)
+const root = createRoot(document.getElementById('root')!);
+root.render(<App />);
