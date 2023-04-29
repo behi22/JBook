751ca58661f56eb4eb2b1c1775c05447e747931f
Behbod Babai <behibabai@gmail.com>
2023-04-29 02:28:11 -0700

    Creating a Markdown Editor for the application

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import { createRoot } from 'react-dom/client';
-import CodeCell from './components/code-cell';
+import TextEditor from './components/text-editor';
 
 const App = () => {
   return (
     <div>
-      <CodeCell />
+      <TextEditor />
     </div>
   );
 };
