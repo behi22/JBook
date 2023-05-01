fd4c0ff3a9f87a4b18d0bc57940f2632f79516b0
Behbod Babai <behibabai@gmail.com>
2023-05-01 02:33:19 -0700

    Designing Redux infrastructure and adding it to the project

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './state';
 import TextEditor from './components/text-editor';
 
 const App = () => {
   return (
-    <div>
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
