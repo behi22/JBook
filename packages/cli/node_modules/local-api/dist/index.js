fc998122a03d35fb26b4d0aff0608f1d64c7f070
Behbod Babai <behibabai@gmail.com>
2023-05-13 02:43:49 -0700

    adding a local-only API

--- packages/cli/node_modules/local-api/dist/index.js
+++ packages/cli/node_modules/local-api/dist/index.js
@@ -1,9 +1,29 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
-const serve = (port, filename, dir) => {
-    console.log('serving traffic on port', port);
-    console.log('saving/fetching cells from', filename);
-    console.log('that file is in dir', dir);
+const express_1 = __importDefault(require("express"));
+const http_proxy_middleware_1 = require("http-proxy-middleware");
+const path_1 = __importDefault(require("path"));
+const cells_1 = require("./routes/cells");
+const serve = (port, filename, dir, useProxy) => {
+    const app = (0, express_1.default)();
+    if (useProxy) {
+        app.use((0, http_proxy_middleware_1.createProxyMiddleware)({
+            target: 'http://localhost:3000',
+            ws: true,
+            logLevel: 'silent',
+        }));
+    }
+    else {
+        const packagePath = require.resolve('local-client/build/index.html');
+        app.use(express_1.default.static(path_1.default.dirname(packagePath)));
+    }
+    app.use((0, cells_1.createCellsRouter)(filename, dir));
+    return new Promise((resolve, reject) => {
+        app.listen(port, resolve).on('error', reject);
+    });
 };
 exports.serve = serve;
